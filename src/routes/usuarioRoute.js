a7e6fb4943ffe3671deafc05bc1e48e4a518d8a7
RadavelliM <henriqueradavellimachado@gmail.com>
2026-02-08 09:39:12 -0300

    middleware jwt

--- src/routes/usuarioRoute.js
+++ src/routes/usuarioRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import UsuarioController from "../controllers/UsuarioController";
+import loginRequired from "../middlewares/loginRequired";
 
 const router = new Router();
 
 router.post("/", UsuarioController.create);
-router.get("/", UsuarioController.index);
+router.get("/", loginRequired, UsuarioController.index);
 router.get("/:id", UsuarioController.show);
 router.put("/:id", UsuarioController.update);
 router.delete("/:id", UsuarioController.delete);
